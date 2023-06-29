b6565e7e1d0a214aad97362dc83b146c90d8c0af
wojciech.nowaczyk <wojciech.nowaczyk@chrobinson.com>
2023-06-29 23:21:10 +0200

    MVC View was implemented

--- AndroidArchitectures/app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.kt
+++ AndroidArchitectures/app/src/main/java/com/example/androidarchitectures/mvc/MVCActivity.kt
@@ -4,12 +4,36 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import com.example.androidarchitectures.R
 
 public class MVCActivity : AppCompatActivity() {
+    private var listValues: ArrayList<String> = ArrayList();
+    lateinit var adapter: ListAdapter;
+    lateinit var list: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mvcactivity)
         title = "MVC Activity"
+
+        list = findViewById(R.id.list);
+        adapter = ArrayAdapter<String>(this, R.layout.row_layout, R.id.listText, listValues)
+
+        list.adapter = adapter
+        list.setOnItemClickListener { adapterView: AdapterView<*>, view1: View, i: Int, l: Long ->
+            Toast.makeText(this, "You clicked item list", Toast.LENGTH_LONG)
+        }
+        var testList : ArrayList<String> = ArrayList();
+        testList.add("Poland");
+        testList.add("Germany")
+        if (testList != null) {
+            setValues(testList)
+        }
+    }
+
+    fun setValues(values : List<String>){
+        listValues.clear();
+        listValues.addAll(values)
     }
 }
